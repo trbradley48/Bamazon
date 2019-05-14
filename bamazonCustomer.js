49dbd2498a60c98430a6f6bf001a11c99c188517
Taylor Bradley <trbradley48@gmail.com>
2019-05-13 23:34:18 -0700

    Completed MVP

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var cTable = require("console.table");
+
+var newStock;
 
 
 var connection = mysql.createConnection({
@@ -16,16 +19,115 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
 });
 
 function start() {
-  connection.query("SELECT * FROM products \G", function(err, results) {
+
+  connection.query("SELECT * FROM products", function (err, results) {
     if (err) throw err;
-    console.log(results);
-    connection.end();
+
+    inquirer
+    .prompt([
+      {
+        name: "start",
+        type: "list",
+        choices: ["Start", "Exit"],
+        message: "Chose 'Start' to take a look at our available products."
+      }
+    ])
+    .then(function(answer) {
+      if (answer.start === "Exit") {
+        connection.end();
+      }
+      else {
+        console.table(results);
+        questions(results);
+      }
+    })
   })
+}
+
+function questions(res) {
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "number",
+        message: "What is ID of the product you would like to purchase?"
+      },
+      {
+        name: "quantity",
+        type: "number",
+        message: "How many units would you like to purchase?"
+      }
+    ])
+    .then(function(answer) {
+      var id = answer.id;
+      var quantity = answer.quantity;
+      var chosenItem;
+
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].item_id === id) {
+          chosenItem = res[i];
+        }
+      }
+
+      if (chosenItem.stock_quantity > 0) {
+        if (quantity <= chosenItem.stock_quantity) {
+          newStock = chosenItem.stock_quantity - quantity;
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: newStock
+              },
+              {
+                item_id: chosenItem.item_id
+              }
+            ],
+            function(error) {
+              if (error) throw error;
+            }
+          );
+          console.log("Your total purchase is: $" + (chosenItem.price * quantity));
+          shopAgain();
+        }
+        else {
+          console.log("Insufficient quantity");
+          start();
+        }
+      }
+      else {
+        console.log("Insufficient quantity");
+        start();
+      }
+    })
+}
+
+function shopAgain() {
+  inquirer
+    .prompt([
+      {
+        name: "continue",
+        type: "list",
+        choices: ["Yes", "No"],
+        message: "Would you like to purchase another item?"
+      }
+    ])
+    .then(function(answer) {
+      if (answer.continue === "Yes") {
+        connection.query("SELECT * FROM products", function(err, response) {
+          if (err) throw err;
+          console.table(response);
+          questions(response);
+        })
+      }
+      else {
+        connection.end();
+      }
+    })
 }
